ddc0c04e0bd65dcb4192efc929eee40a0b142889
dragon2467139 <56919309+dragon2467139@users.noreply.github.com>
2021-08-14 05:45:58 +0800

    Add files via upload

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,8 +1,26 @@
-
 trackList =
     [
-        { name: "Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion", artist: "Maroon 5", src: "musics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).mp3", lrc: "lyrics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).lrc" },
-        { name: "五月天 - 青空未來", artist: "MAYDAY 五月天阿信", src: "musics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.mp3", lrc: "lyrics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.lrc" },
-        { name: "盧廣仲 - 刻在我心底的名字", artist: "盧廣仲 ", src: "musics/盧廣仲 Crowd Lu 【刻在我心底的名字 Your Name Engraved Herein】 Official Music Video （刻在你心底的名字電影主題曲）.mp3", lrc: "lyrics/盧廣仲 - 刻在我心底的名字.lrc" },
-        { name: "Ed Sheeran - Bad Habits", artist: "Ed Sheeran", src: "musics/Ed Sheeran - Bad Habits [Official Video].mp3" }
+        {
+            name: "Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion",
+            artist: "Maroon 5",
+            src: "musics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).mp3",
+            lrc: "lyrics/Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion (Official Music Video).lrc"
+        },
+        {
+            name: "五月天 - 青空未來",
+            artist: "MAYDAY 五月天阿信",
+            src: "musics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.mp3",
+            lrc: "lyrics/MAYDAY 五月天阿信 [ 青空未來 Future ] Official Music Video.lrc"
+        },
+        {
+            name: "盧廣仲 - 刻在我心底的名字",
+            artist: "盧廣仲 ",
+            src: "musics/盧廣仲 Crowd Lu 【刻在我心底的名字 Your Name Engraved Herein】 Official Music Video （刻在你心底的名字電影主題曲）.mp3",
+            lrc: "lyrics/盧廣仲 - 刻在我心底的名字.lrc"
+        },
+        {
+            name: "Ed Sheeran - Bad Habits",
+            artist: "Ed Sheeran",
+            src: "musics/Ed Sheeran - Bad Habits [Official Video].mp3"
+        }
     ];
